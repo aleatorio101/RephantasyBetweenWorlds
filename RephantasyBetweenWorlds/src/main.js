f88e5afda88000e127daeae4882799f5e63f1c7d
Gabriel Gnoatto <gnoattogabriel100@gmail.com>
2025-06-07 00:01:53 -0300

    acho q agora vai

--- RephantasyBetweenWorlds/src/main.js
+++ RephantasyBetweenWorlds/src/main.js
@@ -3,10 +3,10 @@ import { MenuScene } from './scenes/MenuScene.js';
 import { GameScene } from './scenes/GameScene.js';
 import FicharioScene from './scenes/FicharioScene.js';
 import { CreditsScene } from './scenes/CreditsScene.js';
-import  BattleScene_dg  from './scenes/BattleScene_dg.js';
-import  BattleScene_floresta  from './scenes/BattleScene_floresta.js';
-import  BattleScene_dg_boss  from './scenes/BattleScene_dg_boss.js';
-import  BattleScene_FinalBoss  from './scenes/BattleScene_FinalBoss.js';
+import BattleScene_dg from './scenes/BattleScene_dg.js';
+import BattleScene_floresta from './scenes/BattleScene_floresta.js';
+import BattleScene_dg_boss from './scenes/BattleScene_dg_boss.js';
+import BattleScene_FinalBoss from './scenes/BattleScene_FinalBoss.js';
 import { FlorestaScene } from './scenes/FlorestaScene.js';
 import { QuartoScene } from './scenes/QuartoScene.js';
 import { SalaScene } from './scenes/SalaScene.js';
@@ -20,8 +20,7 @@ const config = {
     height: 720,
     backgroundColor: '#000000',
     pixelArt: true,
-   // scene: [BootScene, MenuScene, GameScene, CreditsScene, BattleScene_dg,FlorestaScene, QuartoScene, SalaScene, CavernaScene, BattleScene_floresta, BattleScene_dg_boss, BattleScene_FinalBoss, FicharioScene],
-    scene: [BattleScene_dg],
+    scene: [BootScene, MenuScene, GameScene, CreditsScene, BattleScene_dg, FlorestaScene, QuartoScene, SalaScene, CavernaScene, BattleScene_floresta, BattleScene_dg_boss, BattleScene_FinalBoss, FicharioScene],
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
